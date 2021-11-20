9de7014cdc684cc092f75fe9f8d5d74413f87742
oualid hamri <94682505+walidhmri@users.noreply.github.com>
2021-11-20 22:13:20 +0100

    Add files via upload

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+// Software Name
+$name = 'phpSocial';
+
+// Software Author
+$author = 'phpSocial';
+
+// Software URL
+$url = 'https://phpsocial.com';
+
+// Software Version
+$version = '6.8.0';
+?>
