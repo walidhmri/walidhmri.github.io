50e5f0fa308c7932818e05e742adb8a8f82aa5c3
oualid hamri <94682505+walidhmri@users.noreply.github.com>
2022-01-09 22:21:51 +0100

    Update simplyCountdown.js

--- python/js/simplyCountdown.js
+++ python/js/simplyCountdown.js
@@ -115,11 +115,11 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
-                hours: 0,
-                minutes: 0,
+                year: 2022,
+                month: 1,
+                day: 20,
+                hours: 10,
+                minutes: 30,
                 seconds: 0,
                 words: {
                     days: 'day',
