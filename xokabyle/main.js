e50fe5e1c2194c3a0cf6cfa8570d1948cd3db050
Oualid Hamri <94682505+walidhmri@users.noreply.github.com>
2023-03-24 15:23:04 +0100

    Update main.js

--- xokabyle/main.js
+++ xokabyle/main.js
@@ -1,7 +1,17 @@
 let round = 'x';
 let title = document.querySelector('.title')
+let a3iwed = document.querySelector('.titlereload')
 let sqr = [];
 // Vous pouvez utiliser ce code gratuitement ** Oualid Hamri
+
+function a3awed() {
+    a3iwed.innerHTML = 'Mba3d 3 n les secondes ad i3awed wurar'
+    document.getElementById('but').style.visibility = 'visible';
+
+    setTimeout(() => {
+        document.location.reload();
+    }, 3000);
+}
 function yervah() {
 
     for (let i = 1; i < 10; i++) {
@@ -9,62 +19,47 @@ function yervah() {
     }
     if (sqr[1] == sqr[2] && sqr[2] == sqr[3] && sqr[1] != '') {
         title.innerHTML = 'Yervah ' + sqr[1];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
+        a3awed();
     }
     else if (sqr[4] == sqr[5] && sqr[5] == sqr[6] && sqr[4] != '') {
         title.innerHTML = 'Yervah  ' + sqr[4];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
-
+        a3awed();
     }
     else if (sqr[7] == sqr[8] && sqr[8] == sqr[9] && sqr[7] != '') {
         title.innerHTML = 'Yervah ' + sqr[7];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
-
-
+        a3awed();
     }
     else if (sqr[1] == sqr[4] && sqr[4] == sqr[7] && sqr[1] != '') {
         title.innerHTML = 'Yervah ' + sqr[1];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
-
+        a3awed();
     }
     else if (sqr[4] == sqr[5] && sqr[5] == sqr[6] && sqr[4] != '') {
         title.innerHTML = 'Yervah ' + sqr[4];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
-
-
+        a3awed();
     }
     else if (sqr[7] == sqr[8] && sqr[8] == sqr[9] && sqr[7] != '') {
         title.innerHTML = 'Yervah ' + sqr[7];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
+        a3awed();
     }
     else if (sqr[2] == sqr[5] && sqr[5] == sqr[8] && sqr[2] != '') {
         title.innerHTML = 'Yervah ' + sqr[2];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
+        a3awed();
     }
     else if (sqr[3] == sqr[6] && sqr[6] == sqr[9] && sqr[3] != '') {
         title.innerHTML = 'Yervah ' + sqr[3];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
+        a3awed();
     }
     else if (sqr[1] == sqr[5] && sqr[5] == sqr[9] && sqr[1] != '') {
         title.innerHTML = 'Yervah ' + sqr[1];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
+        a3awed();
     }
     else if (sqr[3] == sqr[5] && sqr[5] == sqr[7] && sqr[3] != '') {
         title.innerHTML = 'Yervah ' + sqr[3];
-        document.getElementById('but').style.visibility = 'visible';
-        exitFunc();
+        a3awed();
     }
-    else if (sqr[1] != '' && sqr[2] != '' && sqr[3] != '' && sqr[4] != '' && sqr[5] != '' && sqr[6] != '' &&  sqr[7] != '' && sqr[8] != '' && sqr[9] != '') {
+    else if (sqr[1] != '' && sqr[2] != '' && sqr[3] != '' && sqr[4] != '' && sqr[5] != '' && sqr[6] != '' && sqr[7] != '' && sqr[8] != '' && sqr[9] != '') {
         document.getElementById('but').style.visibility = 'visible';
+        a3awed();
     }
 
 
